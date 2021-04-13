ba9a1f99b6f457b68f75105baef54270162d9249
Svetlana Vassileva <72747194+svetlilaloli@users.noreply.github.com>
2021-04-13 12:33:13 +0100

    Add files via upload

---
+++ TriFunction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TriFunction
+{
+    class TriFunction
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+            //Func<string, int, bool> isEqualOrGrater = IsEqualOrGrater;
+            Console.WriteLine(FirstEqualOrGrater(names, n, IsEqualOrGrater));
+        }
+        static string FirstEqualOrGrater(string[] names, int n, Func<string, int, bool> isEqualOrGrater)
+        {
+            foreach (string name in names)
+            {
+                if (isEqualOrGrater(name, n))
+                {
+                    return name;
+                }
+            }
+            return null;
+        } 
+        static bool IsEqualOrGrater(string name, int n)
+        {
+            int sum = 0;
+            foreach (char ch in name)
+            {
+                sum += ch;
+            }
+            if (sum >= n)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
